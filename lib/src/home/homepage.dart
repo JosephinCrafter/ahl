3089b33839dfd5105e147a11dc9cee1f17cdd215
JosephinCrafter <razafindrakotojosephin@gmail.com>
2023-12-05 22:18:53 +0300

    add: Promotional banner at the top of the home screen

--- lib/src/home/homepage.dart
+++ lib/src/home/homepage.dart
@@ -4,6 +4,8 @@ import 'package:ahl/src/home/welcoming/welcoming.dart';
 import 'package:ahl/src/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 /// Home page
 
 class HomePage extends StatelessWidget {
@@ -48,13 +50,100 @@ class HomePage extends StatelessWidget {
           // Here are placed all components that build the entire
           // HomePage
           body: ListView(
-            children: const [
-              HeroHeaderView(),
-              WelcomingView(),
+            children: [
+              PromotionBar(
+                backgroundColor:
+                    Theme.of(context).colorScheme.tertiaryContainer,
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      WidgetSpan(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: Paddings.small),
+                          child: Icon(
+                            Icons.build_circle_rounded,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onTertiaryContainer,
+                          ),
+                        ),
+                      ),
+                      TextSpan(
+                        text: AppLocalizations.of(context)!
+                            .inConstructionPromotionalMessage,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              const HeroHeaderView(),
+              const WelcomingView(),
             ],
           ),
         );
       },
     );
+  }
+}
+
+class PromotionBar extends StatefulWidget {
+  const PromotionBar({
+    super.key,
+    required this.child,
+    this.isShown = true,
+    this.backgroundColor,
+  });
+
+  final Widget child;
+  final Color? backgroundColor;
+  final bool isShown;
+
+  @override
+  State<PromotionBar> createState() => _PromotionBarState();
+}
+
+class _PromotionBarState extends State<PromotionBar> {
+  _PromotionBarState();
+
+  late bool isShown;
+
+  @override
+  void initState() {
+    super.initState();
+    isShown = widget.isShown;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isShown
+        ? Container(
+            color: widget.backgroundColor,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                widget.child,
+                IconButton(
+                  onPressed: close,
+                  icon: const Icon(
+                    Icons.close,
+                  ),
+                ),
+              ],
+            ),
+          )
+        : const SizedBox.shrink();
+  }
+
+  void close() => setState(() {
+        isShown = false;
+      });
+
+  void open() {
+    if (isShown == true) {
+      return;
+    } else {
+      setState(() => isShown = true);
+    }
   }
 }
