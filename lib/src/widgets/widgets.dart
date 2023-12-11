9a4248814043a5c4e15db8e3046ac2737e342937
JosephinCrafter <razafindrakotojosephin@gmail.com>
2023-12-11 16:44:25 +0300

    removed unused import

--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -1,7 +1,6 @@
 import 'package:ahl/src/ahl_barrel.dart';
+import 'package:ahl/src/home/homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:transparent_image/transparent_image.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
