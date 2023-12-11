247d972c6e8a7b5f5a4932fe0f0e38d52b25fa71
JosephinCrafter <razafindrakotojosephin@gmail.com>
2023-12-11 16:40:46 +0300

    setup actions gathering

--- lib/src/widgets/actions.dart
+++ lib/src/widgets/actions.dart
@@ -1,9 +1,51 @@
 part of 'widgets.dart';
 
+/// A short hand to get website tabs.
+///
+/// The website tabs like home, prayers, and over tabs are called action. They
+/// are accessed via the [AppBar] or [Footer].
+///
+/// To get localized map string, use [actionLocalized] function that takes a
+/// [BuildContext] argument and return a map with the localized title and the
+/// corresponding route name as value.
 class ActionsLists {
   ActionsLists._();
 
-  static final List<Widget> actions = <Widget>[
+  /// Return a map of localized action denomination and their routeName as Value.
+  ///
+  /// Call this function inside a build method to get the corresponding route
+  /// an action.
+  ///
+  /// Outside a [build] method, consider using the [actions].
+  static Map<String, String?> actionLocalized(BuildContext context) {
+    Map<String, String?> actions = <String, String?>{
+      AppLocalizations.of(context)!.aboutUs: null,
+      AppLocalizations.of(context)!.homeText: HomePage.routeName,
+      AppLocalizations.of(context)!.news: null,
+      AppLocalizations.of(context)!.ourProjects: null,
+      AppLocalizations.of(context)!.prayers: null,
+    };
+
+    return actions;
+  }
+
+  /// A simple map of string that has as key action names and as value route
+  /// names.
+  static Map<String, String?> actions = {
+    "about_us": null,
+    "home": HomePage.routeName,
+    "news": null,
+    "our_projects": null,
+    "prayers": null,
+  };
+
+  /// A List of actions as widget.
+  ///
+  /// This list was optimized for the app drawer. For specific implementation
+  /// like in footer, consider building actionsWidgets from [ActionsLists.actions]
+  /// or [ActionsLists.actionLocalized] to make mapping between actions and
+  /// Widgets.
+  static final List<Widget> actionsWidgets = <Widget>[
     Builder(
       builder: (context) => Padding(
         padding: const EdgeInsets.symmetric(vertical: Paddings.small),
